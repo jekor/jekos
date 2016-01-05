52012fa836d475168fc563916d8273b24af53723
Chris Forno <jekor@jekor.com>
2016-01-05 16:09:00 +0800

    Add AWS JavaScript SDK to deployment of jekos.org.

--- jekos.org/builder.sh
+++ jekos.org/builder.sh
@@ -1,3 +1,6 @@
 source $stdenv/setup
 
 cp -r $src/www $out
+chmod +w $out
+mkdir $out/aws-sdk
+cp $awssdk $out/aws-sdk/application.javascript
--- jekos.org/default.nix
+++ jekos.org/default.nix
@@ -1,6 +1,4 @@
-{ stdenv, bash
-}:
-
+{ stdenv, fetchurl, bash }:
 
 stdenv.mkDerivation {
   name = "jekos.org";
@@ -8,4 +6,8 @@ stdenv.mkDerivation {
   buildInputs = [ bash ];
   inherit bash;
   builder = ./builder.sh;
+  awssdk = fetchurl {
+    url = "https://raw.githubusercontent.com/aws/aws-sdk-js/47f6e3d77b58997d8e5af6c80a0709280c4c54dc/dist/aws-sdk.min.js";
+    sha256 = "188wd2dbipqw3br6hdv3yfscnmgyc732scjmaf09gff1hzcxp7rm";
+  };
 }
