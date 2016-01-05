a06d5831a63778726a81adcbf093277cc40098a9
Chris Forno <jekor@jekor.com>
2016-01-05 16:26:25 +0800

    Cleaning up things I'm not using anymore.

--- ui/builder.sh
+++ ui/builder.sh
@@ -16,24 +16,6 @@ popd
 sed -e '/{css}/{r css/login.css' -e 'd}' < login.html > $out/login.html
 
 cp index.html $out/index.html
-
-# pushd css
-
-# cat <(stylus < common.styl) <(stylus < login.styl) <(stylus < appstore.styl) > $out/bundle.css
-
-# popd
-
-# pushd js
-
-# For now I'm relying on npm to build bundle.js outside of Nix.
-# When ready to try npm2nix, see node-packages-generated.nix in the
-# top-level directory of nixpkgs for an example of how it seems to be
-# used.
-
-# uglifyjs bundle.js --mangle --screw-ie8 -o $out/bundle.js
-# cp bundle.js $out/bundle.js
 cp js/bundle.js $out/bundle.js
-
-# popd
 
 cp -a img $out/
